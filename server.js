c886d338c3935f9b2c54ddbe721132695eb37a2c
croc4-20 <119327900+croc4-20@users.noreply.github.com>
2024-10-05 23:08:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -22,6 +22,44 @@ app.use('/miniGames/slitherSnake/slither/dist', express.static(path.join(__dirna
 io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
 
+  socket.on('customEvent', (data) => {
+    const { opt, data: { type, packet } } = data;
+
+    // Log the received data
+    console.log('Received event:', { opt, type, packet });
+
+    // Handle different commands based on 'opt'
+    switch (opt) {
+      case config.CMD_INIT_ACK:
+        console.log('CMD_INIT_ACK received');
+        // Handle the init acknowledgement
+        handleInitAck(socket, packet);
+        break;
+
+      case config.CMD_SYNC_MAIN_COORD:
+        console.log('CMD_SYNC_MAIN_COORD received');
+        // Handle the main coordinates synchronization
+        handleSyncMainCoord(socket, packet);
+        break;
+
+      case config.CMD_SYNC_OTHER_COORD:
+        console.log('CMD_SYNC_OTHER_COORD received');
+        // Handle the other coordinates synchronization
+        handleSyncOtherCoord(socket, packet);
+        break;
+
+      case config.CMD_LOSE_CONNECT:
+        console.log('CMD_LOSE_CONNECT received');
+        // Handle the lost connection scenario
+        handleLoseConnect(socket, packet);
+        break;
+
+      default:
+        console.log('Unknown command received:', opt);
+        break;
+    }
+  });
+
   // Handle Slither game events
   socket.on('slitherEvent', (data) => {
     // Process Slither game data
