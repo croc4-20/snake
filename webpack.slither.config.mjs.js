ab0a7dde52bed505f53cf5d1d5ef5f3c65c5b9fa
croc4-20 <119327900+croc4-20@users.noreply.github.com>
2024-10-06 23:50:24 +0200

    Update webpack.slither.config.mjs.js

--- webpack.slither.config.mjs.js
+++ webpack.slither.config.mjs.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  context: path.resolve(__dirname, 'web'),
+  context: path.resolve(__dirname, 'dist'),
   entry: './main.ts',
   output: {
     filename: 'slither.bundle.js',
