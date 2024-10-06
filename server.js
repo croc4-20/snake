3f67f1433d7c4a22f2a0b4ffdc3826cb44c5865f
croc4-20 <119327900+croc4-20@users.noreply.github.com>
2024-10-06 15:03:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -81,7 +81,11 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-const PORT = process.env.PORT || 9999;
+cconst PORT = process.env.PORT; 
+if (!PORT) {
+  throw new Error('No PORT environment variable found');
+}
+
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
