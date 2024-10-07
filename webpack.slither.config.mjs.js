fa69db0c7ae0c076e83bffa98725dd22c37a245d
croc4-20 <119327900+croc4-20@users.noreply.github.com>
2024-10-07 09:27:47 +0200

    Update webpack.slither.config.mjs.js

--- webpack.slither.config.mjs.js
+++ webpack.slither.config.mjs.js
@@ -18,7 +18,7 @@ export default {
     filename: 'slither.bundle.js',
     path: path.resolve(__dirname, 'dist/slither'),
     clean: true,
-    publicPath: '/slither/'
+    publicPath: 'dist/slither'
   },
   module: {
     rules: [
