486155bb11c1917c938c6d73d6d7631897e54633
croc4-20 <119327900+croc4-20@users.noreply.github.com>
2024-10-07 13:51:43 +0200

    f

--- build/server/index.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-import { startServer } from './game';
-startServer();
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zZXJ2ZXIvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsWUFBWSxDQUFDO0FBRWIsT0FBTyxFQUFFLFdBQVcsRUFBRSxNQUFNLFFBQVEsQ0FBQztBQUVyQyxXQUFXLEVBQUUsQ0FBQyJ9
