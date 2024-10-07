e01b83875b7e86868a664218a732638f2922fabd
croc4-20 <119327900+croc4-20@users.noreply.github.com>
2024-10-07 13:30:00 +0200

    f

--- build/server/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-import { startServer } from './game.js'; // Use the ES module import syntax
-
-startServer(); // Call the function directly
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxZQUFZLENBQUM7O0FBRWIsaUNBQXFDO0FBRXJDLElBQUEsa0JBQVcsR0FBRSxDQUFDIn0=
